411faa52a7f6d1dfa1a2642fceb3f05f83427f68
Zaphodious <achythlook@blakwurm.com>
2021-08-20 23:42:46 -0800

    Make the whole fucker work

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,33 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
+	import type init from './pokittypes/init'
+	import { PokitOS } from './pokittypes/pokit';
 	export let name: string;
+	export let pokitinit: typeof init
+	export let canvas: HTMLCanvasElement
+
+	console.log(pokitinit)
+	let initret = pokitinit({canvas})
+
+	let fuckyou: HTMLElement
+	onMount(()=>{
+		fuckyou.appendChild(canvas)
+	}) 
+
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<p bind:this={fuckyou}></p>
+	{#await initret}
+	<p>
+		fak
+	</p>
+	{:then [engine, starter] }
+	<p>{engine}</p>
+	<p>{starter()}</p>
+	{/await}
 </main>
 
 <style>
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import App from './App.svelte';
 
+let canvas = document.createElement('canvas')
+canvas.width = 320
+canvas.height = 320
+let pokitinit = window['pokitinit']
+
+console.log('pokitinit', pokitinit)
+
 const app = new App({
 	target: document.body,
 	props: {
-		name: 'world'
+		name: 'world',
+		canvas,
+		pokitinit
 	}
 });
 
