46ece620ab3bd5ff79555f6c91d99cf25c28d86e
Zaphodious <achythlook@blakwurm.com>
2021-08-21 17:42:57 -0800

    Make the clasp function

--- src/App.svelte
+++ src/App.svelte
@@ -1,33 +1,41 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import type init from './pokittypes/init'
-	import { PokitOS } from './pokittypes/pokit';
+	import type { PokitOS } from './pokittypes/pokit';
 	export let name: string;
 	export let pokitinit: typeof init
-	export let canvas: HTMLCanvasElement
+
+	let pokit: PokitOS
+	
+	let kickoff: () => Promise<void>
+	let activated = false
+
+	let canvas: HTMLCanvasElement
 
 	console.log(pokitinit)
 	let initret = pokitinit({canvas})
 
-	let fuckyou: HTMLElement
-	onMount(()=>{
-		fuckyou.appendChild(canvas)
+	onMount(async ()=>{
+		let [one, two] = await pokitinit({canvas})
+		pokit = one as PokitOS
+		kickoff = two as () => Promise<void>
 	}) 
+
+	async function clasp_button_press() {
+		if (kickoff) {
+			activated=true
+			kickoff()
+		}
+	}
 
 </script>
 
+<div id='clasp-top' class:hidden={activated}>
+	<button class:active={kickoff} on:click="{clasp_button_press}">Y</button>
+</div>
+<div id='clasp-bottom' class:hidden={activated}></div>
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-	<p bind:this={fuckyou}></p>
-	{#await initret}
-	<p>
-		fak
-	</p>
-	{:then [engine, starter] }
-	<p>{engine}</p>
-	<p>{starter()}</p>
-	{/await}
+	<canvas width=320 height=320 bind:this={canvas}></canvas>
 </main>
 
 <style>
@@ -36,6 +44,55 @@
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
+	}
+	#clasp-top, #clasp-bottom {
+		position: fixed;
+		width: 100%;
+		background-color: transparent;
+		background-image: url('../img/clasp.svg');
+		background-repeat: no-repeat;
+		background-size: cover;
+		z-index: 1000;
+  		transition: top 1s, bottom 1s, left 1s, right 1s;
+	}
+	#clasp-top {
+		top: 0;
+		right: 0;
+		left: 0;
+		bottom: 45vh;
+		background-position: bottom;
+		z-index: 2100;
+	}
+	#clasp-top.hidden {
+		top: -50vh;
+		bottom: 100vh;
+	}
+	#clasp-bottom {
+		top:14vh;
+		right:0;
+		left: 0;
+		bottom: 0;
+		background-position: top;
+	}
+	#clasp-bottom.hidden {
+		top: 100vh;
+		bottom: -14vh;
+	}
+	#clasp-top button {
+		position: relative;
+		top: 25vh;
+		left: 40vw;
+		width: 20vw;
+		height: 20vw;
+		background-image: url('../img/button_red.svg');
+		background-color: transparent;
+		background-size: contain;
+		background-repeat: no-repeat;
+		border: none;
+		color: transparent;
+	}
+	#clasp-top button.active {
+		background-image: url('../img/button_green.svg');
 	}
 
 	h1 {
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,11 @@
 import App from './App.svelte';
 
-let canvas = document.createElement('canvas')
-canvas.width = 320
-canvas.height = 320
 let pokitinit = window['pokitinit']
-
-console.log('pokitinit', pokitinit)
 
 const app = new App({
 	target: document.body,
 	props: {
 		name: 'world',
-		canvas,
 		pokitinit
 	}
 });
